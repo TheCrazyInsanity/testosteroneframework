8e0c64af175cb1af9659d56b37799c69e6ce7acb
TheCrazyInsanity <inventricks06@gmail.com>
2022-11-01 20:57:00 -0400

    Fixed non index.js urls, and somehow a bug thats my fault

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http'); // Import Node.js core module
 const fs = require('fs')
 const url = require('url');
 var server = http.createServer(function (req, res) {   //create web server
+//var retarded = true
 console.log(req.url)
 console.log(url.parse(req.url, true).query)
 console.log(url.parse(req.url, true))
@@ -10,24 +11,37 @@ if(forg == "/"){
     forg = ""
 }
 try{
+var retarded = false
+if((fs.existsSync(`${__dirname}/servershit${url.parse(forg, true).pathname}`)) && !(fs.existsSync(`${__dirname}/servershit${url.parse(forg, true).pathname}/index.js`))){
+    res.end(fs.readFileSync(`${__dirname}/servershit${url.parse(forg, true).pathname}`))
+    retarded = true
+}else{
+if(retarded == false){
 if (!(url.parse(forg, true).pathname == undefined)){
+`${__dirname}/servershit${url.parse(forg, true).pathname}`
 res.end(require(`${__dirname}/servershit${url.parse(forg, true).pathname}/index.js`).exportshit(url.parse(req.url, true).query))
 }else{
 res.end(require(`${__dirname}/servershit/index.js`).exportshit(url.parse(req.url, true).query))    
 }
 var resolve = require('resolve');
-
+}
 var path = null
+if(retarded == false){
 if (!(url.parse(forg, true).pathname == undefined)){
     path = resolve.sync(`${__dirname}/servershit${url.parse(forg, true).pathname}/index.js`);
 }else{
     path = resolve.sync(`${__dirname}/servershit/index.js`);  
 }
 
+
 console.log("Path to module found:", path);
 
 if (require.cache[path]){
     delete require.cache[path];
+}
+}else{
+    retarded = false
+}
 }
 } catch(e){
     console.log("Somebody went to a wrong page, or maybe they fucking broke something lmfao")
